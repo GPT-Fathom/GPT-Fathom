2cb112bf234c6a48f45b8069cb66520ed184b714
Shen Zheng <shenz2@illinois.edu>
2023-11-06 20:04:48 -0600

    bug fix

--- evals/registry.py
+++ evals/registry.py
@@ -83,7 +83,7 @@ CHAT_MODELS = {
     "gpt-3.5",
     "gpt-3.5-frontend",
     "gpt-3.5-turbo",
-    "gpt-3.5-turbo-16k"，
+    "gpt-3.5-turbo-16k",
     "gpt-3.5-turbo-0301",
     "gpt-3.5-turbo-0613",
     "gpt-4",
@@ -93,6 +93,7 @@ CHAT_MODELS = {
     "gpt-4-32k",
     "gpt-4-0613",
     "gpt-4-32k-0314",
+    "gpt-4-1106-preview"
 }
 
 
